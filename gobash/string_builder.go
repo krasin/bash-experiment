7305899dcab7baba773438f59475e176fcad9436
Ivan Krasin <imkrasin@gmail.com>
2011-01-13 18:21:31 +0300

    Add StringBuilder

---
+++ gobash/string_builder.go
@@ -0,0 +1,77 @@
+package gobash
+
+import (
+  "fmt"
+  "utf8"
+)
+
+const (
+  stringBuilderInitialCapacity = 10
+)
+
+type StringBuilder struct {
+  runes []int
+  cnt int
+}
+
+func NewStringBuilder() *StringBuilder {
+  sb := new(StringBuilder)
+  sb.runes = make([]int, stringBuilderInitialCapacity)
+  return sb
+}
+
+func (sb *StringBuilder) At(index int) int {
+  if index < 0 {
+    panic(fmt.Sprintf("StringBuilder.At(index=%d): index must be non-negative", index))
+  }
+  if index >= sb.cnt {
+    panic(fmt.Sprintf("StringBuilder.At(index=%d): index out of range. Current length is %d", index, sb.cnt))
+  }
+  return sb.runes[index]
+}
+
+func (sb *StringBuilder) Add(rune int) {
+  sb.ensureCapacity(sb.cnt + 1)
+  sb.runes[sb.cnt] = rune
+  sb.cnt++
+}
+
+func (sb *StringBuilder) Append(runes []int) {
+  sb.ensureCapacity(sb.cnt + len(runes))
+  for i, v := range runes {
+    sb.runes[sb.cnt + i] = v
+  }
+  sb.cnt += len(runes)
+}
+
+func (sb *StringBuilder) String() string {
+	runes := sb.runes[:sb.cnt]
+    length := 0
+	for _, v := range runes {
+		length += utf8.RuneLen(v)
+	}
+	data := make([]byte, length)
+	cur := data
+	for _, v := range runes {
+		rlen := utf8.RuneLen(v)
+		utf8.EncodeRune(cur[0:rlen], v)
+		cur = cur[rlen:]
+	}
+	return string(data)
+}
+
+func (sb *StringBuilder) ensureCapacity(capacity int) {
+  if len(sb.runes) >= capacity {
+    return
+  }
+  newlen := len(sb.runes) * 2
+  if newlen < capacity {
+    newlen = capacity
+  }
+  runes := make([]int, newlen)
+  for i, v := range sb.runes {
+    runes[i] = v
+  }
+  sb.runes = runes
+}
+
