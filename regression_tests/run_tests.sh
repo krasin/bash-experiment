8ffbb564b8370890f31236677eb504039ab0c6bb
Ivan Krasin <imkrasin@gmail.com>
2011-01-14 20:05:19 +0000

    Add a stub for test runner

---
+++ regression_tests/run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOT=`dirname $0`/..
+BASH_EXE="$ROOT/bash-4.1/bash"
+GOBASH_EXE="$ROOT/gobash/gobash"
+
+echo "Root: " $ROOT
+echo "Bash executable: " $BASH_EXE
+echo "Gobash executable: " $GOBASH_EXE
+
+
