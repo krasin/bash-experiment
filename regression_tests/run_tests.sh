9421c6b49263b1a0f8cd434a49858161b10f99a4
Ivan Krasin <imkrasin@gmail.com>
2011-01-14 20:37:52 +0000

    Add run_regression_test function. It just currently not executing

--- regression_tests/run_tests.sh
+++ regression_tests/run_tests.sh
@@ -8,4 +8,19 @@ echo "Root: " $ROOT
 echo "Bash executable: " $BASH_EXE
 echo "Gobash executable: " $GOBASH_EXE
 
+function run_regression_test() {
+  echo -n "Running test $1 ... "
+}
+
+for t in r_*.sh
+do
+  run_regression_test $t
+  TEST_RESULT=$?
+  if [ "$TEST_RESULT"="0" ]; then
+    echo Ok
+  else
+    echo "Failed"
+    exit $TEST_RESULT
+  fi
+done
 
