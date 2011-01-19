30dfe7f1cd7a34ba2999cd60c5a6e41ea2027ee8
Ivan Krasin <imkrasin@gmail.com>
2011-01-19 15:13:57 +0000

    Remove unused stuff

--- gobash/eval.go
+++ gobash/eval.go
@@ -30,10 +30,6 @@ int reader_loop() {
 
 		code = setjmp(top_level);
 
-
-		if (interactive_shell && signal_is_ignored(SIGINT) == 0)
-			set_signal_handler(SIGINT, sigint_sighandler);
-
 		if (code != NOT_JUMPED) {
 			indirection_level = our_indirection_level;
 
@@ -115,18 +111,6 @@ int parse_command() {
 	char *command_to_execute;
 
 	need_here_doc = 0;
-	run_pending_traps();
-
-	/* Allow the execution of a random command just before the printing of each primary prompt.  If the shell variable
-	   PROMPT_COMMAND is set then the value of it is the command to execute. */
-	if (interactive && bash_input.type != st_string) {
-		command_to_execute = get_string_value("PROMPT_COMMAND");
-		if (command_to_execute)
-			execute_variable_command(command_to_execute, "PROMPT_COMMAND");
-
-		if (running_under_emacs == 2)
-			send_pwd_to_eterm();	/* Yuck */
-	}
 
 	current_command_line_count = 0;
 	r = yyparse();
@@ -142,7 +126,6 @@ int parse_command() {
 int read_command() {
 	SHELL_VAR *tmout_var;
 	int tmout_len, result;
-	SigHandler *old_alrm;
 
 	set_current_prompt_level(1);
 	global_command = (COMMAND *) NULL;
@@ -152,27 +135,8 @@ int read_command() {
 	tmout_len = 0;
 	old_alrm = (SigHandler *) NULL;
 
-	if (interactive) {
-		tmout_var = find_variable("TMOUT");
-
-		if (tmout_var && var_isset(tmout_var)) {
-			tmout_len = atoi(value_cell(tmout_var));
-			if (tmout_len > 0) {
-				old_alrm = set_signal_handler(SIGALRM, alrm_catcher);
-				alarm(tmout_len);
-			}
-		}
-	}
-
-	QUIT;
-
 	current_command_line_count = 0;
 	result = parse_command();
-
-	if (interactive && tmout_var && (tmout_len > 0)) {
-		alarm(0);
-		set_signal_handler(SIGALRM, old_alrm);
-	}
 
 	return (result);
 }
