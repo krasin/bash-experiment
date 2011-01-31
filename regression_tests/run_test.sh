61396adbac7df0c199cb48cce29c3e4d5552372c
Ivan Krasin <imkrasin@gmail.com>
2011-01-31 15:42:39 -0800

    Fix a typo return -> exit

--- regression_tests/run_test.sh
+++ regression_tests/run_test.sh
@@ -18,13 +18,13 @@ echo "Running bash.."
 $BASH_EXE $TEST_SCRIPT >/dev/null 2> $BASH_STDERR
 BASH_RESULT=$?
 if [ ! $BASH_RESULT -eq 0 ]; then
-  return $BASH_RESULT
+  exit $BASH_RESULT
 fi
 echo "Running gobash..."
 $GOBASH_EXE $TEST_SCRIPT >/dev/null 2> $GOBASH_STDERR
 GOBASH_RESULT=$?
 if [ ! $GOBASH_RESULT -eq 0 ]; then
-  return $GOBASH_RESULT
+  exit $GOBASH_RESULT
 fi
 diff $BASH_STDERR $GOBASH_STDERR
 DIFF_RESULT=$?
